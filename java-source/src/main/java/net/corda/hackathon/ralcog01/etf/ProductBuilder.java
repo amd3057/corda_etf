04977c82473c46ad368f43bcb618b3fa38396542
amd3057 <amd3057@gmail.com>
2018-01-16 17:42:06 -0500

    API

--- java-source/src/main/java/net/corda/hackathon/ralcog01/etf/ProductBuilder.java
+++ java-source/src/main/java/net/corda/hackathon/ralcog01/etf/ProductBuilder.java
@@ -3,8 +3,9 @@ package net.corda.hackathon.ralcog01.etf;
 import net.corda.core.contracts.Amount;
 import net.corda.core.identity.AbstractParty;
 
-import java.util.Collections;
-import java.util.Currency;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public enum ProductBuilder {
 
@@ -16,6 +17,27 @@ public enum ProductBuilder {
                       "Apple", "Information Technology", "NYSE", ProductState.ACTIVE.name(),
                      10000.00, 10000.00, Collections.EMPTY_MAP, owner, "APPL.X", price, quantity);
          }
+        if ("ETF".equals(ticker)) {
+            Map<String,Integer> maps = new HashMap<>();
+            maps.put("AAPL",100);
+            maps.put("MSFT",100);
+            maps.put("AMZN",100);
+            maps.put("FB",100);
+            maps.put("BRK.B",100);
+
+            return new Product("SNY5","SNY5123", "Equity", "ETF",
+                    "SNY5", "SNP 5", "NYSE", ProductState.ACTIVE.name(),
+                    10000.00, 10000.00, maps, owner, "SNY5.X", price, quantity);
+        }
          return null;
+    }
+
+   public static Product createETF(final String ticker, final Integer quantity, List<Product> underlying,AbstractParty owner) {
+
+
+            return new Product("SNY5","SNY5Sd", "Equity", "ETF",
+                    "SNP5", "Information Technology", "NYSE", ProductState.ACTIVE.name(),
+                    10.00, 10.00, underlying.stream().collect(Collectors.toMap(Product::getTicker, Product::getQuantity)), owner, "SNY5.X", 10.00, quantity);
+
     }
 }
