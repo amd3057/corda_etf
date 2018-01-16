6e173b64bcc2b7b6f42cdf631d67f506fc3cd0f4
ritam <ritam.banik@gmail.com>
2018-01-16 14:40:54 -0500

    RB: Updated states

---
+++ java-source/src/main/java/net/corda/hackathon/ralcog01/etf/DeliverBasketContract.java
@@ -0,0 +1,49 @@
+package net.corda.hackathon.ralcog01.etf;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+import net.corda.core.identity.AbstractParty;
+import com.google.common.collect.ImmutableList;
+import java.security.PublicKey;
+import java.util.List;
+
+// Replace TemplateContract's definition with:
+public class DeliverBasketContract implements Contract {
+
+    public static final String DELIVER_BASKET_CONTRACT_ID = "net.corda.hackathon.ralcog01.etf.DeliverBasketContract";
+
+    // Our Create command.
+    public static class Create implements CommandData {
+    }
+
+    @Override
+    public void verify(LedgerTransaction tx) {
+        final CommandWithParties<Create> command = requireSingleCommand(tx.getCommands(), Create.class);
+
+        requireThat(check -> {
+            // Constraints on the shape of the transaction.
+            check.using("Input state of type basket should be consumed when delivering a basket.", !tx.getInputs().isEmpty());
+            check.using("There should be one output state of type basket.", tx.getOutputs().size() == 1);
+
+            // IOU-specific constraints.
+            final Basket out = tx.outputsOfType(Basket.class).get(0);
+            final AbstractParty lender = out.getIssuer();
+            final AbstractParty borrower = out.getOwner();
+            check.using("The lender and the borrower cannot be the same entity.", lender != borrower);
+
+            // Constraints on the signers.
+            final List<PublicKey> signers = command.getSigners();
+            check.using("There must be two signers.", signers.size() == 2);
+            check.using("The issuer and owner must be signers.", signers.containsAll(
+                    ImmutableList.of(borrower.getOwningKey(), lender.getOwningKey())));
+
+            return null;
+        });
+
+    }
+}
---
+++ java-source/src/main/java/net/corda/hackathon/ralcog01/etf/DeliverBasketFlow.java
@@ -0,0 +1,65 @@
+package net.corda.hackathon.ralcog01.etf;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.StateAndContract;
+import net.corda.core.flows.CollectSignaturesFlow;
+import net.corda.core.flows.FinalityFlow;
+import net.corda.core.flows.FlowException;
+import net.corda.core.flows.FlowSession;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+
+import java.security.PublicKey;
+import java.util.List;
+
+public class DeliverBasketFlow extends OrderBaseFlow {
+
+    private final Basket basket;
+    private final Party newOwner;
+
+    public DeliverBasketFlow(Basket basket, Party newOwner) {
+        this.basket = basket;
+        this.newOwner = newOwner;
+    }
+
+    @Override
+    @Suspendable
+    public SignedTransaction call() throws FlowException {
+        // We retrieve the notary identity from the network map.
+        final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+        // We create a transaction builder.
+        final TransactionBuilder txBuilder = new TransactionBuilder();
+        txBuilder.setNotary(notary);
+
+        // We create the transaction components.
+        Basket newBasket = new Basket(this.basket.getIssuer(), this.newOwner, this.basket.getProducts(), this.basket.getReqProduct(), this.basket.getLinearId());
+        StateAndContract outputContractAndState = new StateAndContract(newBasket, DeliverBasketContract.DELIVER_BASKET_CONTRACT_ID);
+        List<PublicKey> requiredSigners = ImmutableList.of(getOurIdentity().getOwningKey());
+        Command cmd = new Command<>(new DeliverBasketContract.Create(), requiredSigners);
+
+
+// We add the items to the builder.
+        txBuilder.withItems(outputContractAndState, cmd);
+
+// Verifying the transaction.
+        txBuilder.verify(getServiceHub());
+
+// Signing the transaction.
+        final SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+// Creating a session with the other party.
+        FlowSession otherpartySession = initiateFlow(newOwner);
+
+// Obtaining the counterparty's signature.
+        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(
+                signedTx, ImmutableList.of(otherpartySession), CollectSignaturesFlow.tracker()));
+
+// Finalising the transaction.
+        subFlow(new FinalityFlow(fullySignedTx));
+
+        return null;
+    }
+}
